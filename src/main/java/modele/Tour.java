de5e21243c7b15bde71a8da4c957431b47c31370
Khomenko Stepan <stepan.khomenko@etu.univ-orleans.fr>
2018-09-28 14:30:31 +0200

    4

--- src/main/java/modele/Tour.java
+++ src/main/java/modele/Tour.java
@@ -12,9 +12,12 @@ public class Tour implements Piece{
 
     public boolean deplacementValide(Case aCase, char xf, int yf) {
 
+        CouleurPiece adversaire = couleur == CouleurPiece.BLANCHE ? CouleurPiece.NOIRE : CouleurPiece.BLANCHE;
+
         if (aCase.getRow() == xf){
             for (int i = aCase.getColumn(); i <= yf; i++){
-                if (plateau.caseOccupeePar(couleur, Case.getCase(xf, i))) {
+                if (plateau.caseOccupeePar(couleur, Case.getCase(xf, i)) ||
+                    plateau.caseOccupeePar(adversaire, Case.getCase(xf, i))) {
                     return false;
                 }
             }
@@ -23,7 +26,8 @@ public class Tour implements Piece{
         else
             if ((aCase.getColumn() == yf)) {
                 for (int i = aCase.getRow(); i <= xf; i++){
-                    if (plateau.caseOccupeePar(couleur, Case.getCase((char) i, yf))) {
+                    if (plateau.caseOccupeePar(couleur, Case.getCase((char) i, yf)) ||
+                        plateau.caseOccupeePar(adversaire, Case.getCase(xf, i))) {
                         return false;
                     }
                 }
