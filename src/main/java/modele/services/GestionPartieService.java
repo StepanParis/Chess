d29f9e321bf0e49bc0901fdf0b2dc604859e9e28
Khomenko Stepan <stepan.khomenko@etu.univ-orleans.fr>
2018-10-04 16:33:44 +0200

    6

--- src/main/java/modele/services/GestionPartieService.java
+++ src/main/java/modele/services/GestionPartieService.java
@@ -1,4 +1,8 @@
 package modele.services;
 
 public interface GestionPartieService {
+
+    long creerUnePartie(String pseud) throws DejaImpliqueDansUnePartie, PseudeNonConnecteExceptio;
+
+    void rejoindUnePartie
 }
