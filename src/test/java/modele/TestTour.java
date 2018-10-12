ae46699f11ec84fc1e2eb26f77ea5d1e2b1eb86a
Sony <stepan.khomenko@gmail.com>
2018-10-12 10:18:21 +0200

    13

--- src/test/java/modele/TestTour.java
+++ src/test/java/modele/TestTour.java
@@ -13,22 +13,19 @@ public class TestTour {
 
         QuestionnerPlateau plateau = new QuestionnerPlateau() {
             public boolean caseOccupeePar(CouleurPiece couleurPiece, Case caseP) {
-                Case a5 = Case.getCase('a', 5);
-                if ((caseP == a5) && couleurPiece == CouleurPiece.NOIRE) {
+                Case c5 = Case.getCase('c', 5);
+                if ((caseP == c5) && couleurPiece == CouleurPiece.NOIRE) {
                     return true;
                 }
                 return false;
             }
-
             public boolean pieceNonMenacee(CouleurPiece couleur, Case aCase) {
                 return false;
             }
         };
 
-
         Tour tour = new Tour(null, CouleurPiece.NOIRE);
-
-        Assert.assertFalse(tour.deplacementValide(Case.getCase('d', 5), 'd', 8));
+        Assert.assertFalse(tour.deplacementValide(Case.getCase('c', 1), 'c', 5));
     }
 
 }
